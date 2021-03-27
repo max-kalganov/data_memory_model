fa850a8f2a0b311a2804621298bf06d330cbdcdd
max <maksim.kalganov@gmail.com>
2021-03-27 13:24:55 +0300

    improve model, use Adam as ooptimizer with gradient clipping

--- models/simple_lstm_model.py
+++ models/simple_lstm_model.py
@@ -5,40 +5,48 @@ import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.layers import Dense, Bidirectional, LSTM, TimeDistributed
+from tensorflow.python.keras.layers import Dense, Bidirectional, LSTM, TimeDistributed, Dropout
 from dataset_generators.simple_memory_data_generator import SimpleMemoryDataGenerator
 
 
 @gin.configurable
 class SimpleLSTMModel:
-    def __init__(self, seq_len: int, items_len: int, features_range: int, batch_size, train_test_split, train_val_split):
+    def __init__(self, seq_len: int, items_len: int, features_range: int, batch_size: int,
+                 num_of_epochs: int, train_test_split: float, train_val_split: float):
         self.items_len = items_len
         self.seq_len = seq_len
         self.features_range = features_range
         self.train_test_split = train_test_split
         self.train_val_split = train_val_split
         self.batch_size = batch_size
+        self.num_of_epochs = num_of_epochs
         self.model = self.set_model()
 
     def set_model(self):
         input_layer = Input(shape=(self.seq_len, self.items_len, ))
-        lstm1 = LSTM(units=64, activation='relu', return_sequences=True, input_shape=(self.seq_len, self.items_len))(input_layer)
-        lstm2 = LSTM(units=64, activation='relu', return_sequences=True)(lstm1)
-        lstm3 = LSTM(units=64, activation='relu', return_sequences=True)(lstm2)
+        lstm1 = Bidirectional(LSTM(units=64,
+                                   activation='relu',
+                                   return_sequences=True,
+                                   input_shape=(self.seq_len, self.items_len))
+                              )(input_layer)
+        lstm2 = Bidirectional(LSTM(units=64, activation='relu', return_sequences=True))(lstm1)
+        lstm3 = Bidirectional(LSTM(units=64, activation='relu', return_sequences=True))(lstm2)
 
         layer1_1 = TimeDistributed(Dense(units=64, activation='relu'))(lstm3)
-        layer1_2 = TimeDistributed(Dense(units=64, activation='relu'))(layer1_1)
+        layer1_2 = TimeDistributed(Dense(units=64, activation='sigmoid'))(layer1_1)
         y1_output = TimeDistributed(Dense(units=1, activation='sigmoid'), name='y1_output')(layer1_2)
 
         layer2_1 = TimeDistributed(Dense(units=64, activation='relu'))(lstm3)
-        layer2_2 = TimeDistributed(Dense(units=64, activation='relu'))(layer2_1)
+        dropout_1 = Dropout(0.2)(layer2_1)
+        layer2_2 = TimeDistributed(Dense(units=64, activation='relu'))(dropout_1)
         layer2_3 = TimeDistributed(Dense(units=64, activation='relu'))(layer2_2)
         y2_output = TimeDistributed(Dense(units=self.features_range, activation='softmax'), name='y2_output')(layer2_3)
 
         # Define the model with the input layer and a list of output layers
         model = Model(inputs=input_layer, outputs=[y1_output, y2_output])
 
-        optimizer = tf.keras.optimizers.SGD(lr=0.001)
+        # optimizer = tf.keras.optimizers.SGD(lr=0.001)
+        optimizer = tf.keras.optimizers.Adam(clipvalue=0.5)
         model.compile(optimizer=optimizer,
                       loss={'y1_output': 'binary_crossentropy', 'y2_output': self.missed_value_loss},
                       metrics=['accuracy'])
@@ -75,7 +83,8 @@ class SimpleLSTMModel:
         return train_X, (train_Y_1, train_Y_2), val_X, (val_Y_1, val_Y_2), test_X, (test_Y_1, test_Y_2)
 
     def train(self, train_X: np.array, train_Y: np.array, val_X: np.array, val_Y: np.array):
-        history = self.model.fit(train_X, train_Y, epochs=50, batch_size=self.batch_size, validation_data=(val_X, val_Y))
+        history = self.model.fit(train_X, train_Y, epochs=self.num_of_epochs, batch_size=self.batch_size,
+                                 validation_data=(val_X, val_Y))
         return history
 
     def missed_value_loss(self, y_true: tf.Tensor, y_pred: tf.Tensor):
