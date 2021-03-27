66c16014b5a93d94dfd4cdacf6276bdebe06e732
max <maksim.kalganov@gmail.com>
2021-03-27 12:56:36 +0300

    correct config, fix passing dataset in the model

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run_model():
         model = m.SimpleLSTMModel()
         train_X, train_Y, val_X, val_Y, test_X, test_Y = model.get_train_val_test()
         model.train(train_X, train_Y, val_X, val_Y)
-        #model.evaluate(test_X, test_Y)
+        model.evaluate(test_X, test_Y)
 
 
 if __name__ == '__main__':
--- models/simple_lstm_model.py
+++ models/simple_lstm_model.py
@@ -26,53 +26,67 @@ class SimpleLSTMModel:
         lstm2 = LSTM(units=64, activation='relu', return_sequences=True)(lstm1)
         lstm3 = LSTM(units=64, activation='relu', return_sequences=True)(lstm2)
 
-        layer1_1 = Dense(units=64, activation='relu')(lstm3)
-        layer1_2 = Dense(units=64, activation='relu')(layer1_1)
-        y1_output = Dense(units=1, activation='sigmoid', name='y1_output')(layer1_2)
+        layer1_1 = TimeDistributed(Dense(units=64, activation='relu'))(lstm3)
+        layer1_2 = TimeDistributed(Dense(units=64, activation='relu'))(layer1_1)
+        y1_output = TimeDistributed(Dense(units=1, activation='sigmoid'), name='y1_output')(layer1_2)
 
-        layer2_1 = Dense(units=64, activation='relu')(lstm3)
-        layer2_2 = Dense(units=64, activation='relu')(layer2_1)
-        layer2_3 = Dense(units=64, activation='relu')(layer2_2)
-        y2_output = Dense(units=self.features_range, activation='softmax', name='y2_output')(layer2_3)
+        layer2_1 = TimeDistributed(Dense(units=64, activation='relu'))(lstm3)
+        layer2_2 = TimeDistributed(Dense(units=64, activation='relu'))(layer2_1)
+        layer2_3 = TimeDistributed(Dense(units=64, activation='relu'))(layer2_2)
+        y2_output = TimeDistributed(Dense(units=self.features_range, activation='softmax'), name='y2_output')(layer2_3)
 
         # Define the model with the input layer and a list of output layers
         model = Model(inputs=input_layer, outputs=[y1_output, y2_output])
 
         optimizer = tf.keras.optimizers.SGD(lr=0.001)
         model.compile(optimizer=optimizer,
-                      loss={'y1_output': 'binary_crossentropy', 'y2_output': self.missed_value_loss})
+                      loss={'y1_output': 'binary_crossentropy', 'y2_output': self.missed_value_loss},
+                      metrics=['accuracy'])
         print(model.summary())
         return model
 
     def get_train_val_test(self) -> Tuple[np.array, np.array, np.array, np.array, np.array, np.array]:
         generator = SimpleMemoryDataGenerator()
         full_dataset_X = []
-        full_dataset_Y = []
+        full_dataset_Y_1 = []
+        full_dataset_Y_2 = []
+
         for X, Y in generator:
             full_dataset_X.append(X)
-            full_dataset_Y.append(Y)
+            full_dataset_Y_1.append(Y[0])
+            full_dataset_Y_2.append(Y[1])
 
-        full_train_X, test_X, full_train_Y, test_Y = train_test_split(full_dataset_X, full_dataset_Y,
-                                                                      test_size=self.train_test_split)
+        full_dataset_X = np.concatenate(full_dataset_X)
+        full_dataset_Y_1 = np.concatenate(full_dataset_Y_1)
+        full_dataset_Y_2 = np.concatenate(full_dataset_Y_2)
 
-        train_X, val_X, train_Y, val_Y = train_test_split(full_train_X, full_train_Y, test_size=self.train_val_split)
+        full_train_X, test_X, full_train_Y_1, test_Y_1, full_train_Y_2, test_Y_2 = train_test_split(
+            full_dataset_X,
+            full_dataset_Y_1,
+            full_dataset_Y_2,
+            test_size=self.train_test_split)
 
-        return train_X, train_Y, val_X, val_Y, test_X, test_Y
+        train_X, val_X, train_Y_1, val_Y_1, train_Y_2, val_Y_2 = train_test_split(
+            full_train_X,
+            full_train_Y_1,
+            full_train_Y_2,
+            test_size=self.train_val_split)
+
+        return train_X, (train_Y_1, train_Y_2), val_X, (val_Y_1, val_Y_2), test_X, (test_Y_1, test_Y_2)
 
     def train(self, train_X: np.array, train_Y: np.array, val_X: np.array, val_Y: np.array):
-
-        history = self.model.fit(train_X, train_Y,
-                                 epochs=50, batch_size=self.batch_size, validation_data=(val_X, val_Y))
+        history = self.model.fit(train_X, train_Y, epochs=50, batch_size=self.batch_size, validation_data=(val_X, val_Y))
         return history
 
-    def missed_value_loss(self, y_true, y_pred):
-        loss = 0
-        print(y_true)
+    def missed_value_loss(self, y_true: tf.Tensor, y_pred: tf.Tensor):
+        y_true_one_hot = tf.reshape(tf.one_hot(y_true, self.features_range),
+                                    shape=(self.batch_size, self.seq_len, self.features_range))
+        cross_entropy_res = tf.keras.losses.categorical_crossentropy(y_true_one_hot, y_pred)
 
-        if y_true != np.nan:
-            y_true = tf.one_hot(y_true, self.features_range)
-            loss = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
-        return loss
+        y_true_labels = tf.reshape(y_true != -1, shape=(self.batch_size, -1))
+        correct_cross_entropy_res = tf.math.reduce_sum(cross_entropy_res[y_true_labels])
+        return correct_cross_entropy_res
 
     def evaluate(self, test_X: np.array, test_Y: np.array):
-        print(self.model.evaluate(test_X, test_Y))
+        print("Evaluating test results:")
+        self.model.evaluate(test_X, test_Y, batch_size=self.batch_size)
